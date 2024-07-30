11367a4483838d14750167ec90312b2884724c80
Dane Ahern <danielaherniv@gmail.com>
2024-07-30 18:56:48 -0400

    can get user by ID

--- app/routers/user.py
+++ app/routers/user.py
@@ -16,4 +16,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)
     return new_user
 
 
+@router.get("/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+
+
 
