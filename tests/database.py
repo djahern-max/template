048c176111c6f389139cb8f97f4e0b2c190d5ebf
Dane Ahern <danielaherniv@gmail.com>
2024-09-08 13:26:26 -0400

    testing login functionality working 16:03

---
+++ tests/database.py
@@ -0,0 +1,35 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.main import app
+from app import models  
+from app.database import get_db
+
+# Database URL
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Guitar0123!@localhost:5432/fastapi_test'
+
+# Create engine and session
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture(scope="module")
+def session():
+    models.Base.metadata.drop_all(bind=engine)
+    models.Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Create client fixture
+@pytest.fixture(scope="module")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,45 +1,13 @@
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.main import app
-from app import schemas, models  # models import needed
-from app.database import get_db
-
-# Database URL
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Guitar0123!@localhost:5432/fastapi_test'
+from app import schemas
+from .database import client, session
+from app.config import settings
+import jwt
 
 def test_root(client):
     res = client.get("/")
     print(res.json().get('message'))
     assert res.json().get('message') == "Welcome to ryze!"
     assert res.status_code == 200
-
-
-# Create engine and session
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-@pytest.fixture
-def session():
-    models.Base.metadata.drop_all(bind=engine)
-    models.Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# Create client fixture
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
 
 # Test for creating user
 def test_create_user(client):
@@ -48,4 +16,11 @@ def test_create_user(client):
     print(res.json())
     assert new_user.email == "test@gmail.com"
     assert res.status_code == 200
+
+def test_login_user(client):
+    res = client.post(
+        "/auth/login", data={"username": "test@gmail.com", "password": "123456"})
+    print(res.json())
+    assert res.status_code == 200
+
 
